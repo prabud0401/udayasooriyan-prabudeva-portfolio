028f652987ed8da188dc50e0eaf961f5b97ee3e5
Udayasooriyan Prabudeva <prabud0401@gmail.com>
2024-04-29 14:54:56 +0530

    updated

---
+++ src/components/about/AboutMe.vue
@@ -0,0 +1,44 @@
+<script>
+export default {
+	setup() {
+		return {
+			bios: [
+				{
+					id: 1,
+					bio:
+						"I'm a passionate computer enthusiast and student. I love exploring the vast world of technology, coding, and everything related to computers. From programming to hardware, I find joy in understanding how things work in the digital realm. I'm always eager to learn and stay updated with the latest trends in the tech industry.",
+				},
+				// {
+				// 	id: 2,
+				// 	bio:
+				// 		'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil vel illum asperiores dignissimos cumque quibusdam et fugiat voluptatem nobis suscipit explicabo, eaque consequatur nesciunt, fugit eligendi corporis laudantium adipisci soluta?',
+				// },
+			],
+		};
+	},
+};
+</script>
+
+<template>
+	<div class="block sm:flex sm:gap-10 mt-10 sm:mt-20">
+		<!-- About profile image -->
+		<div class="w-full sm:w-1/4 mb-7 sm:mb-0">
+			<img
+				src="@/assets/images/profile.jpeg"
+				class="rounded-xl w-96"
+				alt=""
+			/>
+		</div>
+
+		<!-- About details -->
+		<div class="w-full sm:w-3/4 text-left">
+			<p
+				v-for="bio in bios"
+				:key="bio.id"
+				class="font-general-regular mb-4 text-ternary-dark dark:text-ternary-light text-lg"
+			>
+				{{ bio.bio }}
+			</p>
+		</div>
+	</div>
+</template>
---
+++ src/data/socialLinks.js
@@ -0,0 +1,39 @@
+export const socialLinks = [
+	{
+		id: 1,
+		name: 'Website',
+		icon: 'globe',
+		url: 'https://prabud0401.github.io/udayasooriyan-prabudeva-portfolio/',
+	},
+	{
+		id: 2,
+		name: 'GitHub',
+		icon: 'github',
+		url: 'https://github.com/prabud0401',
+	},
+	{
+		id: 3,
+		name: 'Twitter',
+		icon: 'twitter',
+		url: 'https://twitter.com/im_dprabu',
+	},
+	{
+		id: 4,
+		name: 'LinkedIn',
+		icon: 'linkedin',
+		url: 'https://www.linkedin.com/in/prabud0401',
+	},
+	{
+		id: 5,
+		name: 'YouTube',
+		icon: 'youtube',
+		url: 'https://www.youtube.com/c/im_motiver',
+	},
+	{
+		id: 6,
+		name: 'WhatsApp',
+		icon: 'message-circle',
+		url: 'https://call.whatsapp.com/voice/O57cbVEUhPrdaOkOUOyGjY',
+	},
+
+];
